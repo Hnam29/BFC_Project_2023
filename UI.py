4d30bfbbcd20519c960475306e11e25715894513
Hector29 <114966508+Hnam29@users.noreply.github.com>
2023-08-29 16:24:58 +0700

    commit upload

---
+++ UI.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+def UI():
+    st.markdown("""  
+            
+ <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+<div class="container-fluid">
+    <div class="card shadow">
+      <div class="card-body">
+        <div class="card">
+          <div class="card-body">
+            <h3 class="text-secondary", style="text-align:center"> ⚛ BFC PRODUCTS ⚛ </h3>
+            <h4 class="text-secondary", style="text-align:center"> <i>Bring the best quality to our stakeholders</i> | <b>Always creative to be a leader</b> </h3>
+          </div>
+        </div>        
+      </div>
+    </div>
+</div>        
+<style>
+div[data-testid="stExpander"] div[role="button"] p
+{
+    font-size: 1.1rem;
+}
+ 
+</style>
+ """, unsafe_allow_html=True)
